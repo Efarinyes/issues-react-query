94499140ce47ab9407570f65e740488c5a6568e7
Eduard  Farinyes Gasalla <Eduard@gmail.com>
2022-12-30 20:21:07 +0100

    React Query - infinite Scroll

--- src/api/githupApi.ts
+++ src/api/githupApi.ts
@@ -5,6 +5,6 @@ import axios from 'axios';
 export const githubApi = axios.create({
     baseURL: 'https://api.github.com/repos/facebook/react',
     headers: {
-        Authorization: 'Bearer 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'
+        Authorization: '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'
     }
 })
